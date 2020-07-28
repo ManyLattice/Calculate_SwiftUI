5c26733b2ecd84ab349b1844ccca7949bdbfa249
Mikhail <manylattice7742@yandex.kz>
2020-07-28 14:46:49 +0600

    Initial Commit

---
+++ Calculate_SwiftUITests/Calculate_SwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  Calculate_SwiftUITests.swift
+//  Calculate_SwiftUITests
+//
+//  Created by Михаил Гудзикевич on 7/28/20.
+//  Copyright © 2020 Михаил Гудзикевич. All rights reserved.
+//
+
+import XCTest
+@testable import Calculate_SwiftUI
+
+class Calculate_SwiftUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
